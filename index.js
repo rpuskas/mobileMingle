1618d89744cd6a306a2baa9a7f8e62c591e7f293
Ryan Puskas <rpuskaspublic@gmail.com>
2011-12-23 14:06:29 -0500

    rpuskas: adding some quickfunctionality to the points/stories slider

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-var options = {
-	chart: {
-		renderTo: 'container',
-		type: 'column',
-		height: 225
-	}, 
-	tooltip: {
-   enabled: false
-  },
-	legend: {
-		enabled: false	
-	},
-	title: {
-		text: 'User Journey Backlog'
-	},
-	xAxis: {
-		categories: ["Step 1","Step 1","Step 3"]
-	},
-	yAxis: {
-		title: {
-			text: '# of Stories'
-		}
-	},
-	series: [{data:[1,2,4]}]
-};
 
-$(function(){  			
-	var chart = new Highcharts.Chart(options);
+$(function(){  	 
+	
+	var options = {
+		chart: {
+			renderTo: 'container',
+			type: 'column',
+			height: 225
+		}, 
+		tooltip: {
+	   enabled: false
+	  },
+		legend: {
+			enabled: false	
+		},
+		title: {
+			text: 'User Journey Backlog'
+		},
+		xAxis: {
+			categories: ["Step 1","Step 1","Step 3"]
+		},
+		yAxis: {
+			title: {
+				text: '# of Stories'
+			}
+		},
+		series: [{data:[1,2,4]}]
+	};  
+	var chart = new Highcharts.Chart(options);    
+	
+	$( "#slider-stories" ).bind( "change", function(event, ui) {
+		var selectedItem = $(this).val();	   
+		options.yAxis.title.text = (selectedItem == 'Points' ? '# of Points' : '# of Stories');  
+		options.series = (selectedItem == 'Points' ? [{data:[1,2,4]}] : [{data:[5,3,1]}] );    
+		chart = new Highcharts.Chart(options);
+	});
+	
 });
