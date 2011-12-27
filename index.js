e291762e846f62d7d681e4904f3e74ba7247d2f6
Ryan Puskas <rpuskaspublic@gmail.com>
2011-12-27 16:00:48 -0500

    rpuskas: adding detail entry for journies

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-
 $(function(){  	 
+	
+	var journies = [];          
+	if(localStorage.journies){
+		journies = JSON.parse(localStorage.journies);
+	}
 	
 	var options = {
 		chart: {
 			renderTo: 'container',
 			type: 'column',
-			height: 225
+			height: 250
 		}, 
 		tooltip: {
 	   enabled: false
@@ -16,23 +20,69 @@ $(function(){
 		title: {
 			text: 'User Journey Backlog'
 		},
-		xAxis: {
-			categories: ["Step 1","Step 1","Step 3"]
+		xAxis: {    
+			categories : journies.map(function(x){ return x.name }) 
 		},
 		yAxis: {
 			title: {
 				text: '# of Stories'
 			}
 		},
-		series: [{data:[1,2,4]}]
+		series: [{data: journies.map(function(x){ return parseInt(x.points) })}]
 	};  
-	var chart = new Highcharts.Chart(options);    
+	var chart = new Highcharts.Chart(options);  
 	
-	$( "#slider-stories" ).bind( "change", function(event, ui) {
+	
+	$( '#viewGraph' ).live( 'pageshow',function(event){
+	   	 
+			console.log('shown');
+			var journies = [];          
+			if(localStorage.journies){
+				journies = JSON.parse(localStorage.journies);
+			}
+
+			window.debug = journies;
+
+			var selectedItem = $(this).val();	   
+			options.yAxis.title.text = (selectedItem == 'Points' ? '# of Points' : '# of Stories');  
+			options.xAxis.categories = journies.map(function(x){ return x.name });
+			options.series = [{data: journies.map(function(x){ return parseInt(x.points) })}];
+			chart = new Highcharts.Chart(options); 
+			
+	});
+	 
+	
+	$( "#slider-stories" ).bind( "change", function(event, ui) {    
+		    
+		console.log('changed');
+		var journies = [];          
+		if(localStorage.journies){
+			journies = JSON.parse(localStorage.journies);
+		}
+		
+		window.debug = journies;
+		
 		var selectedItem = $(this).val();	   
 		options.yAxis.title.text = (selectedItem == 'Points' ? '# of Points' : '# of Stories');  
-		options.series = (selectedItem == 'Points' ? [{data:[1,2,4]}] : [{data:[5,3,1]}] );    
+		options.xAxis.categories = journies.map(function(x){ return x.name });
+		options.series = [{data: journies.map(function(x){ return parseInt(x.points) })}];
 		chart = new Highcharts.Chart(options);
+	});   
+	
+	
+	$( "#add" ).bind( "click", function(event, ui) { 
+
+		var journies = localStorage.journies ? JSON.parse(localStorage.journies) : [];
+		
+		journies.push({
+			name: $("#name").val(),  
+			count: $("#storyCount").val(),
+			points: $("#points").val()
+		});  
+
+		console.log(JSON.stringify(journies));
+		localStorage.journies = JSON.stringify(journies);
+   
 	});
 	
-});
+});    
