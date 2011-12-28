a60d50d2f7aefcf5f664f29400c42ec32226e1a9
Ryan Puskas <rpuskaspublic@gmail.com>
2011-12-28 00:28:09 -0500

    rpuskas: refactoring, adding grid and chart refresh

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-$(function(){  	 
-	
-	var journies = [];          
-	if(localStorage.journies){  
-		
-		journies = JSON.parse(localStorage.journies); 
-		  
-		window.debug = journies;
-		 
-		var totalPoints =  _.reduce(journies, function(total, num){ return total + parseInt(num.points); }, 0);
-		var totalStories = _.reduce(journies, function(total, num){ return total + parseInt(num.count); }, 0); 
-
-	  	$("#totalPoints").html(totalPoints);
-	  	$("#totalStories").html(totalStories);
-	  	$("#totalJournies").html(journies.length); 
-		
+MM = {}   
+MM.Index = {
+	 
+	getJournies : function(){
+		return localStorage.journies ? JSON.parse(localStorage.journies) : [];            		
 	}
+}  
+
+MM.Index.Chart = { 
 	
-	var options = {
+	options : {
 		chart: {
 			renderTo: 'container',
 			type: 'column',
@@ -32,45 +24,72 @@ $(function(){
 			text: 'User Journey Backlog'
 		},
 		xAxis: {    
-			categories : journies.map(function(x){ return x.name }) 
+			categories : MM.Index.getJournies().map(function(x){ return x.name }) 
 		},
 		yAxis: {
 			title: {
 				text: '# of Stories'
 			}
 		},
-		series: [{data: journies.map(function(x){ return parseInt(x.points) })}]
-	};  
-	var chart = new Highcharts.Chart(options);  
+		series: [{data: MM.Index.getJournies().map(function(x){ return parseInt(x.points) })}]
+	},  
 	
-	var loadGraph = function(){
-		
-		var journies = [];          
-		if(localStorage.journies){
-			journies = JSON.parse(localStorage.journies);
-		}
-		
-		window.debug = journies;
-		
-		var selectedItem = $(this).val();	   
-		options.yAxis.title.text = (selectedItem == 'Points' ? '# of Points' : '# of Stories');  
-		options.xAxis.categories = journies.map(function(x){ return x.name });
-		options.series = [{data: journies.map(function(x){ return parseInt(x.points) })}];
-		chart = new Highcharts.Chart(options);
-		
-	}               
+	load : function(){
+
+		var journies = MM.Index.getJournies();
+
+		this.options.xAxis.categories = journies.map(function(x){ return x.name });
+		this.options.series = [{data: journies.map(function(x){ return parseInt(x.points) })}];
+		chart = new Highcharts.Chart(this.options);
+
+	}
 	
+}
+
+MM.Index.Grid = {  
+	
+  refresh : function(){
+
+		var journies = MM.Index.getJournies();
+
+		var totalPoints =  _.reduce(journies, function(total, num){ return total + parseInt(num.points); }, 0);
+		var totalStories = _.reduce(journies, function(total, num){ return total + parseInt(num.count); }, 0); 
+
+	  	$("#totalPoints").html(totalPoints);
+	  	$("#totalStories").html(totalStories);
+	  	$("#totalJournies").html(journies.length);    	
+	}  
+	
+}
+
+$(function(){  	 
+	   
+	var journies = MM.Index.getJournies();  
+	MM.Index.Grid.refresh();
+               
 	$( '#viewGraph' ).live( 'pageshow',function(event){
-	   	loadGraph();
+	
+		MM.Index.Chart.load();
+	
+	}); 
+	
+	$( '#main' ).live('main',function(event){
+	    
+		MM.Index.Grid.refresh()
+	 
 	});
 	  
-	$( "#slider-stories" ).bind( "change", function(event, ui) {
-	    loadGraph();
+	$( "#slider-stories" ).bind( "change", function(event, ui) {  
+		
+		var selectedItem = $(this).val();	   
+		options.yAxis.title.text = (selectedItem == 'Points' ? '# of Points' : '# of Stories');
+		
+		MM.Index.Chart.load();
 	});   
 	
 	$( "#add" ).bind( "click", function(event, ui) { 
 		  
-		var journies = localStorage.journies ? JSON.parse(localStorage.journies) : [];
+		var journies = MM.Index.getJournies();
 
 		journies.push({
 			name: $("#name").val(),  
@@ -78,8 +97,8 @@ $(function(){
 			points: $("#points").val()
 		});  
 
-		console.log(JSON.stringify(journies));
-		localStorage.journies = JSON.stringify(journies);   
+		localStorage.journies = JSON.stringify(journies); 
+		MM.Index.Grid.refresh();  
 		
 	}); 
 	
