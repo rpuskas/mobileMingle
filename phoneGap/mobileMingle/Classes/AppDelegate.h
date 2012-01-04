2412ebf1418e524a30c5dc7e72b6bd3926f2fb0a
Ryan Puskas <rpuskaspublic@gmail.com>
2012-01-04 00:40:14 -0500

    rpuskas: adding phone gap
    rpuskas: moving textmate project up a level
    
    rpuskas: fixed invalid paths

---
+++ phoneGap/mobileMingle/Classes/AppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.h
+//  mobileMingle
+//
+//  Created by rpuskas on 1/4/12.
+//  Copyright ThoughtWorks Inc. 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#ifdef PHONEGAP_FRAMEWORK
+	#import <PhoneGap/PhoneGapDelegate.h>
+#else
+	#import "PhoneGapDelegate.h"
+#endif
+
+@interface AppDelegate : PhoneGapDelegate {
+
+	NSString* invokeString;
+}
+
+// invoke string is passed to your app on launch, this is only valid if you 
+// edit mobileMingle.plist to add a protocol
+// a simple tutorial can be found here : 
+// http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+
+@property (copy)  NSString* invokeString;
+
+@end
+
